0a48573e227b6418ba9d9aaace96f6640e09f663
Fotohh <66885524+Fotohh@users.noreply.github.com>
2024-02-09 09:55:40 -0800

    Removed trash

--- build.bat
+++
@@ -1,8 +0,0 @@
-g++ -c main.cc -I submodules/imgui
-g++ -c submodules/imgui/imgui_demo.cpp -I submodules/imgui 
-g++ -c submodules/imgui/imgui_draw.cpp -I submodules/imgui
-g++ -c submodules/imgui/imgui_tables.cpp -I submodules/imgui
-g++ -c submodules/imgui/imgui_widgets.cpp -I submodules/imgui
-g++ -c submodules/imgui/imgui.cpp -I submodules/imgui
-g++ -c glad.c -I glad
-g++ -o app.exe imgui_demo.o imgui_draw.o imgui_tables.o imgui_widgets.o imgui.o main.o 
--- user_process.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-struct User {
-    char32_t* uuid;
-
-    char32_t getUserID();
-
-    //do more stuff here
-};
